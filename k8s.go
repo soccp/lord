31f748096c1ca04f32583a01e82e00c14fc18ba5
hd <18511890109@163.com>
2018-11-28 17:14:31 +0800

    k8s seperate from lord

---
+++ k8s.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"github.com/rancher/norman/signal"
+	"github.com/rancher/rancher/k8s"
+
+)
+
+var (
+	VERSION = "dev"
+)
+
+func main() {
+	ctx := signal.SigTermCancelContext(context.Background())
+	_, ctx, _, err := k8s.GetConfig(ctx, "auto", "")
+	if err != nil {
+		return err
+	}
+	
+	os.Unsetenv("KUBECONFIG")
+	
+	<-ctx.Done()
+	
+	return ctx.Err()
+}
