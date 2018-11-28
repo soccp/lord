89a0b9562aacbc0845916843279148d277f07a41
hd <18511890109@163.com>
2018-11-28 18:12:09 +0800

    fix

--- k8s.go
+++ k8s.go
@@ -16,12 +16,12 @@ var (
 func main() {
 	logrus.Info("k8s is starting")
 	ctx := signal.SigTermCancelContext(context.Background())
-	_, ctx, kubeConfig, err := k8s.GetConfig(ctx, "auto", "")
+	embedded, ctx, kubeConfig, err := k8s.GetConfig(ctx, "auto", "")
 	if err != nil {
 		logrus.Infof("create k8s failed %s", err)
 		return
 	}
-	
+	logrus.Info(embedded)
 	logrus.Info(kubeconfig)
 	os.Unsetenv("KUBECONFIG")
 	
